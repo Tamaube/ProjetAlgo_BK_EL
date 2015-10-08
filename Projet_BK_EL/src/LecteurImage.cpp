bc747428a329f4a32a2d11b5e769ae7ebf9a970a
khennicb <benkhen@hotmail.fr>
2015-10-08 21:32:49 +0200

    Presque fini le Read

--- Projet_BK_EL/src/LecteurImage.cpp
+++ Projet_BK_EL/src/LecteurImage.cpp
@@ -10,14 +10,22 @@ LecteurImage::LecteurImage()
         int entier1, entier2;
         string chaine1;
 
+
         fichier >> chaine1 >> tailleX >> tailleY ;  /*on lit jusqu'à l'espace et on stocke ce qui est lu dans la variable indiquée */
 
         unsigned char bit;
-        Pixel **tabPixels;
         tabPixels = malloc(sizeof(void *)*tailleX*tailleY);
-        for (int i = 0; i < tailleX*tailleY; i++) {
-            fichier >> bit;
 
+        for (unsigned int j = 0; j < tailleY; j++) {
+            for (unsigned int i = 0; i < tailleX; i++) {
+
+                fichier >> bit;
+                if(bit == O) {
+                    Pixel tabPixels[i*j] = new Pixel(i, j);
+                }else {
+                    tabPixels[i*j] = nullptr;
+                }
+            }
         }
 
     }
