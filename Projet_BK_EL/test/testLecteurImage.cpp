cea1bf53b9c9e6505e9800c267e7a81ab820e47a
khennicb <benkhen@hotmail.fr>
2015-10-22 22:35:59 +0200

    test + 1 commentaire + 1 fct

---
+++ Projet_BK_EL/test/testLecteurImage.cpp
@@ -0,0 +1,19 @@
+#include "Pixel.h"
+#include <iostream>
+
+using namespace std;
+
+void AfficheTableauPixel(Pixel** tabPixel, unsigned int Dimension_X,unsigned int Dimension_Y, ) {
+    for (unsigned int ligne = 0; ligne < Dimension_Y; ligne++){
+        cout << ligne << "\t(";
+        for (unsigned int col = 0; col < Dimension_X; col++) {
+            if (tabP[col + ligne*Dimension_X].pixelNoir) {
+                cout <<  "1 ";
+            }else {
+                cout <<  "0 ";
+            }
+        }
+        cout << ")" << endl;
+    }
+
+}
